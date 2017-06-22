904a3a84faf4e71b11b72c8434c6a229d89b2df7
Roger Li <email": null>
2017-06-22 10:46:07 -0700

    Implement 'add friend' and 'accept friend request' buttons

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.new(friend_id: params[:friend_id])
+    @friendship = Friendship.new(user_id: params[:user_id], friend_id: params[:friend_id])
     if @friendship.save
       flash[:notice] = "Friend request sent."
       redirect_back(fallback_location: root_path)
@@ -11,6 +11,13 @@ class FriendshipsController < ApplicationController
   end
 
   def update
+    @friendship = Friendship.find_by(id: params[:id])
+    @friendship.update_attributes(accepted: true)
+    if @friendship.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
