280483b3c70c2535a4e6bb48953d3b743978cc5b
Mike Tarkington <tarkmike@gmail.com>
2017-06-23 11:39:05 -0700

    put edit button in parial of listed comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170623140727) do
 
   # These are extensions that must be enabled in order to support this database
@@ -53,9 +52,8 @@ ActiveRecord::Schema.define(version: 20170623140727) do
     t.string "playtime", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.string "image_link"
     t.string "buy_link"
+    t.string "image_link"
   end
 
   create_table "ownerships", force: :cascade do |t|
